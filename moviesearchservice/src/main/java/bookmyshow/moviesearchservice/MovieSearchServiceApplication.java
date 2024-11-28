346ccbdd3dc2079553858fa2f32ecfaadc47508c
rs-rishabh <rs.18rishabh@gmail.com>
2024-11-28 16:50:51 +0530

    added project structure

--- moviesearchservice/src/main/java/bookmyshow/moviesearchservice/MovieSearchServiceApplication.java
+++ moviesearchservice/src/main/java/bookmyshow/moviesearchservice/MovieSearchServiceApplication.java
@@ -1,13 +1,13 @@
-package com.bookmyshow.com.search_service;
+package bookmyshow.moviesearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class MovieSearchServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MovieSearchServiceApplication.class, args);
 	}
 
 }
